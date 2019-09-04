4b2731c2257a50d6659bced6b3f64bd4abc69d47
AnnaShalaginova <anya.shalaginova@gmail.com>
2019-09-04 15:20:01 -0400

    Set up handlebars view engine

--- meadowlark.js
+++ meadowlark.js
@@ -2,6 +2,12 @@ var express = require('express');
 
 var app = express();
 
+// set up handlebars view engine
+var handlebars = require('express-handlebars')
+  .create({ defaultLayout: 'main'});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
 app.set('port', process.env.PORT || 3000);
 
 // custom 404 page
@@ -18,9 +24,25 @@ app.use(function(err, req, res, next){
   res.status(500);
   res.send('500 - Server Error');
 
-})
+});
 
 app.listen(app.get('port'), function(){
   console.log('Express started on http://localhost:' +
     app.get('port') + '; press Ctrl-C to terminate.');
+});
+
+app.get('/', function(req, res){
+  res.type('text/plain');
+  res.send('Meadownlark Travel');
+});
+
+app.get('about', function(req, res){
+  res.type('text/plain');
+  res.send('About Meadowlark Travel');
+});
+
+app.use(function(req, res, next){
+  res.type('text/plain');
+  res.status(404);
+  res.send('404 - Not Found');
 });
