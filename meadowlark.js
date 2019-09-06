18316424111c90520d759235aec8973cf08d5bac
AnnaShalaginova <anya.shalaginova@gmail.com>
2019-09-06 10:58:47 -0400

    Including test condition

--- meadowlark.js
+++ meadowlark.js
@@ -2,47 +2,47 @@ var express = require('express');
 
 var app = express();
 
+var fortune = require('./lib/fortune.js');
+
 // set up handlebars view engine
 var handlebars = require('express-handlebars')
-  .create({ defaultLayout: 'main'});
+	.create({ defaultLayout:'main' });
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
 
-// custom 404 page
-app.use(function(req, res){
-  res.type('text/plain');
-  res.status(404);
-  res.send('404 - Not Found');
+app.use(function(req, res, next){
+	res.locals.showTests = app.get('env') !== 'production' &&
+		req.query.test === '1';
+	next();
 });
 
-// custom 500 page
-app.use(function(err, req, res, next){
-  console.error(err.stack);
-  res.type('text/plain');
-  res.status(500);
-  res.send('500 - Server Error');
+app.use(express.static(__dirname + '/public'));
 
+app.get('/', function(req, res) {
+	res.render('home');
+});
+app.get('/about', function(req,res){
+	var randomFortune =
+		fortuneCookies[Math.floor(Math.random() * fortuneCookies.length)];
+	res.render('about', { fortune: randomFortune });
+});
+
+// 404 catch-all handler (middleware)
+app.use(function(req, res, next){
+	res.status(404);
+	res.render('404');
+});
+
+// 500 error handler (middleware)
+app.use(function(err, req, res, next){
+	console.error(err.stack);
+	res.status(500);
+	res.render('500');
 });
 
 app.listen(app.get('port'), function(){
-  console.log('Express started on http://localhost:' +
-    app.get('port') + '; press Ctrl-C to terminate.');
-});
-
-app.get('/', function(req, res){
-  res.type('text/plain');
-  res.send('Meadownlark Travel');
-});
-
-app.get('about', function(req, res){
-  res.type('text/plain');
-  res.send('About Meadowlark Travel');
-});
-
-app.use(function(req, res, next){
-  res.type('text/plain');
-  res.status(404);
-  res.send('404 - Not Found');
+  console.log( 'Express started on http://localhost:' +
+    app.get('port') + '; press Ctrl-C to terminate.' );
 });
